3388132f41d397bebf77ef3fcd98b34b6c236e38
Catriel Guillen <catriel@maderenovation.com>
2023-12-21 22:51:12 -0300

    fix: deploy command

--- deploy_lightsail.sh
+++ deploy_lightsail.sh
@@ -19,7 +19,7 @@ CONTAINERS_JSON=$(
     },
     "environment": {
       "AWS_ACCESS_KEY_ID": "$AWS_ACCESS_KEY_ID",
-      "AWS_SECRET_ACCESS_KEY":"$AWS_SECRET_ACCESS_KEY",
+      "AWS_SECRET_ACCESS_KEY":"$AWS_SECRET_ACCESS_KEY"
     }
   }
 }
